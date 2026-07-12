921ac18013b4457103b18180636c2ad022041a62
Takuro Tamura <tak.0413.cbr250r@gmail.com>
2026-07-12 15:52:51 +0900

    Add new currency pairs

--- src/main/java/com/takuro_tamura/autofx/domain/model/value/CurrencyPair.java
+++ src/main/java/com/takuro_tamura/autofx/domain/model/value/CurrencyPair.java
@@ -3,5 +3,6 @@ package com.takuro_tamura.autofx.domain.model.value;
 public enum CurrencyPair {
     USD_JPY, EUR_JPY, GBP_JPY, AUD_JPY, NZD_JPY, CAD_JPY,
     CHF_JPY, TRY_JPY, ZAR_JPY, MXN_JPY, EUR_USD, GBP_USD,
-    AUD_USD, NZD_USD,
+    AUD_USD, NZD_USD, HUF_JPY, SEK_JPY, EUR_GBP, AUD_NZD,
+    AUD_CAD, NZD_CAD, NOK_SEK,
 }
